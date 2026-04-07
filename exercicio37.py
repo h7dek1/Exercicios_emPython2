81ac745175a1809fc908386db41156945d0620f6
h7dek1 <hidekioficial@gmail.com>
2026-04-07 10:32:57 -0300

    correção dos valor da variavel n2 e adicionei o "print(termo) dentro do for para mostrar a série
    Fix Fibonacci series calculation and output.

--- exercicio37.py
+++ exercicio37.py
@@ -5,17 +5,20 @@
 #declaração de variaveis
 num_termos: int=0
 n1= 1
-n2= 2
+n2= 1
 
 #inicio
 num_termos= int(input("defina o número de termos da série: "))
 if (num_termos==1) or (num_termos==2):
     print("1")
 else:
-    for count in range(2,num_termos):
+    print(0)
+    print(1)
+    for contador in range(2,num_termos):
         termo= n1 + n2
         n2 = n1
         n1= termo
-        count += 1
+        contador += 1
+        print(termo)
     print(f"A série de {num_termos} vai até: {termo}")
 #fim
